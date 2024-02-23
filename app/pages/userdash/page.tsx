6cba636fddfd9a6d9f91a223df0dbb00ba19ae84
KarwalAnsh <ansh.karwal.ug20@nsut.ac.in>
2024-02-23 19:00:26 +0530

    Created user dashboard

---
+++ app/pages/userdash/page.tsx
@@ -0,0 +1,12 @@
+import UserBar from "@/app/components/userdashcomp/UserBar";
+import React from "react";
+
+const UserDash: React.FC = () => {
+  return (
+    <div>
+      <UserBar />
+    </div>
+  );
+};
+
+export default UserDash;
