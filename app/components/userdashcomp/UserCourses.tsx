8d4904e82088b515f1271a33af7b299a47b217bf
KarwalAnsh <ansh.karwal.ug20@nsut.ac.in>
2024-02-24 17:25:56 +0530

    updated screen for user courses

--- app/components/userdashcomp/UserCourses.tsx
+++ app/components/userdashcomp/UserCourses.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Cards from "../mainpagecomp/Cards";
 import FilterDrawer from "../coursecomp/FilterDrawer";
+import Divider from "../sharedcomps/Divider";
 
 const CardDrawer = () => {
   // Array of image sources for each card
@@ -17,10 +18,20 @@ const CardDrawer = () => {
   return (
     <div className="mt-8 md:mt-16 flex flex-col items-center">
       <div className="pb-5 w-full max-w-screen-xl">
-        <div className="mb-6 flex items-center justify-between w-full bg-yellow-300  p-4">
-          <h1 className="ml-6 text-2xl md:text-4xl font-bold">
+        <div className="mb-4">
+          <h1 className="text-2xl md:text-4xl font-bold text-center">
             Your Purchased Courses
           </h1>
+        </div>
+        <Divider />
+        <div className="mb-6 flex flex-col md:flex-row items-center justify-between w-full py-4 bg-yellow-600">
+          <div className="flex items-center mb-4 ml-4 md:mb-0">
+            <input
+              type="text"
+              placeholder="Search for my courses"
+              className="p-2 w-64 mr-4 border border-gray-300 rounded-xl"
+            />
+          </div>
           <FilterDrawer />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
